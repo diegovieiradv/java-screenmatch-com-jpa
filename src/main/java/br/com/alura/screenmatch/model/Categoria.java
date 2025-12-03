f9e83a246c4663409a7631c306f60e287f972559
Diego Vieira <dvdiego87@gmail.com>
2025-12-03 05:33:09 -0300

    criei nova classe ao project

---
+++ src/main/java/br/com/alura/screenmatch/model/Categoria.java
@@ -0,0 +1,25 @@
+package br.com.alura.screenmatch.model;
+
+public enum Categoria {
+    ACÃO("action"),
+    COMEDIA("comedy"),
+    DRAMA("drama"),
+    ROMANCE("romance"),
+    TERROR("horror"),
+    CRIME("crime"),
+    FICÇÃO("ficcao");
+
+    private String categoriaOmdb;
+
+    Categoria(String categoriaOmdb) {
+        this.categoriaOmdb = categoriaOmdb;
+    }
+    public static Categoria fromSpring(String text){
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Categoria inválida: " + text);
+    }
+}
