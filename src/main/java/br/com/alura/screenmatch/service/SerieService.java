da8875ee805a00c67c383b08ef5d27380d3a2a23
Diego Vieira <dvdiego87@gmail.com>
2025-12-19 02:56:25 -0300

    atualizei menu

--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -20,7 +20,7 @@ public class SerieService {
     @Autowired
     private SerieRepository repositorio;
 
-    public List<SerieDTO> obterTodasAsSeries(){
+    public List<SerieDTO> obterTodasAsSeries() {
         return converteDados(repositorio.findAll());
     }
 
@@ -28,10 +28,10 @@ public class SerieService {
         return converteDados(repositorio.findTop5ByOrderByAvaliacaoDesc());
     }
 
-    private List<SerieDTO> converteDados(List<Serie> series){
-       return series.stream().map(s -> new SerieDTO(s.getId(), s.getTitulo(),
-               s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(),
-               s.getPoster(), s.getSinopse())).collect(Collectors.toList());
+    private List<SerieDTO> converteDados(List<Serie> series) {
+        return series.stream().map(s -> new SerieDTO(s.getId(), s.getTitulo(),
+                s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(),
+                s.getPoster(), s.getSinopse())).collect(Collectors.toList());
     }
 
     public List<SerieDTO> obterLancamentos() {
@@ -40,7 +40,7 @@ public class SerieService {
 
     public SerieDTO obterPorId(Long id) {
         Optional<Serie> serie = repositorio.findById(id);
-        if (serie.isPresent()){
+        if (serie.isPresent()) {
             Serie s = serie.get();
             return new SerieDTO(s.getId(), s.getTitulo(),
                     s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(),
@@ -52,7 +52,7 @@ public class SerieService {
     public List<EpisodioDTO> obterTodasTemporadas(Long id) {
         Optional<Serie> serie = repositorio.findById(id);
 
-        if (serie.isPresent()){
+        if (serie.isPresent()) {
             Serie s = serie.get();
             return s.getEpisodios().stream().map(e -> new EpisodioDTO(e.getTemporada(), e.getNumeroEpisodio(), e.getTitulo()))
                     .collect(Collectors.toList());
@@ -61,7 +61,7 @@ public class SerieService {
     }
 
     public List<EpisodioDTO> obterTemporadasPorNumero(Long id, Long numero) {
-        return repositorio.obterEpisodiosPorTemporada(id , numero)
+        return repositorio.obterEpisodiosPorTemporada(id, numero)
                 .stream().map(e -> new EpisodioDTO(e.getTemporada(), e.getNumeroEpisodio(), e.getTitulo()))
                 .collect(Collectors.toList());
     }
